0dd6e66be9ecfa5ca4d02eb6a41bd34ac3aaefe6
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-07-21 17:37:30 +0530

    Main component

---
+++ components/CodeView/index.tsx
@@ -0,0 +1,24 @@
+import { Row, Col, Typography } from 'antd';
+import { Content } from 'antd/lib/layout/layout';
+const { Title } = Typography;
+import dynamic from 'next/dynamic';
+const Editor = dynamic(() => import(`@monaco-editor/react`), { ssr: false });
+import useConverter from 'hooks/useConverter';
+
+const Codeview = () => {
+	const { handleTypeScriptChange, typescript, javascript } = useConverter();
+	return (
+		<Content>
+			<Row>
+				<Col span={12}>
+					<Editor theme='vs-dark' height='70vh' value={typescript} onChange={handleTypeScriptChange} defaultLanguage='typescript' defaultValue='// paste your typescript interface here' />
+				</Col>
+				<Col span={12}>
+					<Editor theme='vs-dark' height='70vh' defaultLanguage='typescript' value={javascript} defaultValue='// validation code would appear here' />
+				</Col>
+			</Row>
+		</Content>
+	);
+};
+
+export default Codeview;
